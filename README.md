e9598194f6963d7fd4982f5cc2faa840f528b568
Aymeric <aymeric.luneau@gmail.com>
2026-04-03 15:57:50 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sciences ouvertes à Paris 8 : connaissance et besoins
+
+Le dépôt contient les codes utilisés pour le traitement de l'enquête sur les connaissances et les besoins en matière de science ouverte des personnels de recherche de Paris 8.
+
---
+++ open-notebook.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This init script clones a Git repository that contains a Jupyter notebook
+# named `tutorial.ipynb` and opens it in Jupyter Lab at startup
+# Expected parameters : None
+
+
+# Clone repository and give permissions to the onyxia user
+GIT_REPO=bso_p8
+git clone --depth 1 https://github.com/luneauaymeric/${GIT_REPO}.git
+chown -R onyxia:users ${GIT_REPO}/
+
+sudo apt update -y
+#sudo apt install ffmpeg -y
+#sudo apt install libavcodec-extra -y
+
+# Install additional packages if a requirements.txt file is present in the project
+REQUIREMENTS_FILE=${GIT_REPO}/requirements.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
+
+python -m spacy download fr_core_news_md
+python -m spacy download en_core_web_md
+
+
+# Open the relevant notebook when starting Jupyter Lab
+# echo "c.LabApp.default_url = '/lab/tree/${GIT_REPO}/bertopic_tutorial_git.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
